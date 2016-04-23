9a64848b19e3df857a8709fec55308934681de64
ELE712 <zandber2@msu.edu>
2016-04-22 22:06:17 -0400

    Sorting works, saving doesn't
    Saved a bunch of messy attempted file save attempts, thinking it might be a bug with macs, or I'm stupid.

--- 335proj04/databaseform.cpp
+++ 335proj04/databaseform.cpp
@@ -1,6 +1,9 @@
 #include "databaseform.h"
 #include "ui_databaseform.h"
 #include "addrecordform.h"
+#include <QTableView>
+#include <QFile>
+#include <QString>
 
 DatabaseForm::DatabaseForm(QWidget *parent) :
     QMainWindow(parent),
@@ -9,7 +12,9 @@ DatabaseForm::DatabaseForm(QWidget *parent) :
     ui->setupUi(this);
     ui->tableWidget->setRowCount(0);
     ui->tableWidget->setColumnCount(4);
-    ui->tableWidget->
+    QHeaderView* m_header = ui->tableWidget->horizontalHeader(); //headers
+    connect(m_header, SIGNAL(sectionClicked(int)), this, SLOT(on_header_clicked(int))); //header connection
+    //
 }
 
 DatabaseForm::~DatabaseForm()
@@ -27,10 +32,19 @@ void DatabaseForm::on_addRecordButton_clicked()
 void DatabaseForm::addData(std::vector<QString> str_vec){
     int t_row = ui->tableWidget->rowCount();
     ui->tableWidget->setRowCount(t_row+1); //add one row, t_row in correct index
-    ui->tableWidget->setItem(t_row, 0, new QTableWidgetItem(str_vec[0]));
+    ui->tableWidget->setItem(t_row, 0, new QTableWidgetItem(str_vec[0])); //first and last name
     ui->tableWidget->setItem(t_row, 1, new QTableWidgetItem(str_vec[1]));
-    ui->tableWidget->setItem(t_row, 2, new QTableWidgetItem(str_vec[2]));
-    ui->tableWidget->setItem(t_row, 3, new QTableWidgetItem(str_vec[3]));
+
+    QTableWidgetItem* sal_item = new QTableWidgetItem; //item for salary
+    int sal_int = str_vec[2].toInt(); //get integer value
+    sal_item->setData(Qt::EditRole, sal_int); //save int in data
+
+    QTableWidgetItem* year_item = new QTableWidgetItem; //same for year
+    int year_int = str_vec[3].toInt();
+    year_item->setData(Qt::EditRole, year_int);
+
+    ui->tableWidget->setItem(t_row, 2, sal_item); //salary and year
+    ui->tableWidget->setItem(t_row, 3, year_item);
 }
 
 void DatabaseForm::on_okButton_clicked()
@@ -38,4 +52,63 @@ void DatabaseForm::on_okButton_clicked()
     this->close();
 }
 
+void DatabaseForm::on_header_clicked(int index){
+    qDebug("Index clicked: %d", index);
+    ui->tableWidget->sortByColumn(index);
 
+
+}
+
+
+
+void DatabaseForm::on_actionSave_triggered()
+{
+    QFile file("CSV.txt");
+    //QFile t_file("TSV.txt");
+    int row_count = ui->tableWidget->rowCount();
+
+    if (!file.open(QIODevice::WriteOnly)){
+        qDebug("Uh oh");
+        return;
+    }
+
+    QTextStream c_str(&file);
+    for (int i=0; i<row_count; i++){ //each row
+        for (int j=0; j<4; j++){ //each column in row
+            c_str << ui->tableWidget->item(i,j)->text() << ",";
+            //QString test = ui->tableWidget->item(i,j)->text();
+        }
+        c_str << "\n";
+    }
+    qDebug("Got here");
+    file.close();
+
+/*
+    if (t_file.open(QIODevice::WriteOnly | QIODevice::Text)){
+        qDebug("Uh Oh v2");
+        return;
+    }
+    QTextStream t_str(&t_file);
+    for (int i=0; i<row_count; i++){ //each row
+        for (int j=0; j<4; j++){ //each column in row
+            t_str << ui->tableWidget->item(i,j)->text() << ",";
+        }
+        t_str << "\n";
+    }
+    */
+
+
+    /*
+    CommaBuilder cb;
+    TabBuilder tb;
+    int row_count = ui->tableWidget->rowCount();
+    for (int i=0; i<row_count; i++){ //each row
+        for (int j=0; j<4; j++){ //each column in row
+            cb.addItem(ui->tableWidget->item(i,j)->text());
+            tb.addItem(ui->tableWidget->item(i,j)->text());
+        }
+    }
+    //cb.saveFile();
+    //tb.saveFile();
+    */
+}
